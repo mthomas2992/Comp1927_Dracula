e7c4ebab8c9d9b07cde3c4cc00e9720643e53523
mthomas2992 <mthomas2992@gmail.com>
2015-09-25 18:58:54 +1000

    Cleaned up code for submission

--- GameView.c
+++ GameView.c
@@ -8,19 +8,18 @@
 #include "Players.h"
 #include "Map.h"
 
-// #include "Map.h" ... if you decide to use the Map ADT
 struct gameView {
-    PlayerID CurrentPlayer;
-    Round RoundNum;
-    Map europe;
+   PlayerID CurrentPlayer;
+   Round RoundNum;
+   Map europe;
 
-    Player Lord_Godalming;
-    Player Dr_Seward;
-    Player Van_Helsing;
-    Player Mina_Harker;
-    Player Dracula;
+   Player Lord_Godalming;
+   Player Dr_Seward;
+   Player Van_Helsing;
+   Player Mina_Harker;
+   Player Dracula;
 
-    int GameScore;    
+   int GameScore;
 };
 
 
@@ -28,55 +27,55 @@ struct gameView {
 // Creates a new GameView to summarise the current state of the game
 GameView newGameView(char *pastPlays, PlayerMessage messages[])
 {
-    GameView gameView = malloc(sizeof(struct gameView));
-    // INITIALIZE ALL THE THINGS IN THE GAMEVIEW STRUCT
-    // WILL FINISH THIS LAST
-    gameView->RoundNum = 0;
-    gameView->CurrentPlayer = 0;
-    gameView->GameScore = GAME_START_SCORE;
-    gameView->europe = newMap();
+   GameView gameView = malloc(sizeof(struct gameView));
+   // INITIALIZE ALL THE THINGS IN THE GAMEVIEW STRUCT
+   gameView->RoundNum = 0;
+   gameView->CurrentPlayer = 0;
+   gameView->GameScore = GAME_START_SCORE;
+   gameView->europe = newMap();
 
-    gameView->Lord_Godalming = initPlayer(PLAYER_LORD_GODALMING);
-    gameView->Dr_Seward	     = initPlayer(PLAYER_DR_SEWARD);
-    gameView->Van_Helsing    = initPlayer(PLAYER_VAN_HELSING);
-    gameView->Mina_Harker    = initPlayer(PLAYER_MINA_HARKER);
-    gameView->Dracula	     = initPlayer(PLAYER_DRACULA);
+   gameView->Lord_Godalming = initPlayer(PLAYER_LORD_GODALMING);
+   gameView->Dr_Seward	     = initPlayer(PLAYER_DR_SEWARD);
+   gameView->Van_Helsing    = initPlayer(PLAYER_VAN_HELSING);
+   gameView->Mina_Harker    = initPlayer(PLAYER_MINA_HARKER);
+   gameView->Dracula	     = initPlayer(PLAYER_DRACULA);
 
-    int i;
-    int scoreChange=0;
+   int i;
+   int scoreChange=0;
 
-    //// HANDLING PAST PLAYS STRING
-    //// We will probably do this a different way in the final thing,  (maybe)
-    //// but for now it has to be like this because we can't add functions in 
-    //// this ADT  ((  :(  ))
-    for (i=0; pastPlays[i] != '\0' ; i+=7) {
-	if (pastPlays[i] == ' ') i++;
-	if (pastPlays[i] == 'G') {
-            scoreChange = doPlayerTurn(gameView->Lord_Godalming, pastPlays+i);
-        } else if (pastPlays[i] == 'S') {
-            scoreChange = doPlayerTurn(gameView->Dr_Seward, pastPlays+i);
-        } else if (pastPlays[i] == 'H') {
-            scoreChange = doPlayerTurn(gameView->Van_Helsing, pastPlays+i);
-        } else if (pastPlays[i] == 'M') {
-            scoreChange = doPlayerTurn(gameView->Mina_Harker, pastPlays+i);
-        } else if (pastPlays[i] == 'D') {
-            scoreChange = doPlayerTurn(gameView->Dracula, pastPlays+i);
-        }
-        if (scoreChange <= 0) {
-            gameView->GameScore += scoreChange;
-        } else if (scoreChange == 10) deductHealth(gameView->Dracula, 10);
-        else if (scoreChange == 4) {
-            deductHealth(gameView->Dracula, 10);
-            gameView->GameScore -= 6;
-        }
-	gameView->CurrentPlayer++;
-	if (gameView->CurrentPlayer==5) {
-	    gameView->CurrentPlayer = 0;
-            gameView->RoundNum++;
-        }
-    }    
+   //// HANDLING PAST PLAYS STRING
+   //// We will probably do this a different way in the final thing,  (maybe)
+   //// but for now it has to be like this because we can't add functions in
+   //// this ADT  ((  :(  ))
+   for (i=0; pastPlays[i] != '\0' ; i+=7) {
+      if (pastPlays[i] == ' ') i++;
+      if (pastPlays[i] == 'G') { //if Godalmings turn 0
+         scoreChange = doPlayerTurn(gameView->Lord_Godalming, pastPlays+i);
+      } else if (pastPlays[i] == 'S') { //Seward 1
+         scoreChange = doPlayerTurn(gameView->Dr_Seward, pastPlays+i);
+      } else if (pastPlays[i] == 'H') { //Van helsing 2
+         scoreChange = doPlayerTurn(gameView->Van_Helsing, pastPlays+i);
+      } else if (pastPlays[i] == 'M') { //Mine 3
+         scoreChange = doPlayerTurn(gameView->Mina_Harker, pastPlays+i);
+      } else if (pastPlays[i] == 'D') { //DRACUULLLAAA 4
+         scoreChange = doPlayerTurn(gameView->Dracula, pastPlays+i);
+      }
+      if (scoreChange <= 0) {
+         gameView->GameScore += scoreChange;
+      } else if (scoreChange == 10)
+         deductHealth(gameView->Dracula, 10);
+      else if (scoreChange == 4) {
+         deductHealth(gameView->Dracula, 10);
+         gameView->GameScore -= 6;
+      }
+      gameView->CurrentPlayer++;
+      if (gameView->CurrentPlayer==5) {
+         gameView->CurrentPlayer = 0;
+         gameView->RoundNum++;
+      }
+   }
 
-    return gameView;
+   return gameView;
 }
 
 
@@ -84,15 +83,13 @@ GameView newGameView(char *pastPlays, PlayerMessage messages[])
 // Frees all memory previously allocated for the GameView toBeDeleted
 void disposeGameView(GameView toBeDeleted)
 {
-    //COMPLETE THIS IMPLEMENTATION
-    // this should be pretty small, reasonably simple
-    removePlayer( toBeDeleted->Lord_Godalming );
-    removePlayer( toBeDeleted->Dr_Seward );
-    removePlayer( toBeDeleted->Van_Helsing );
-    removePlayer( toBeDeleted->Mina_Harker );
-    removePlayer( toBeDeleted->Dracula );
-    disposeMap( toBeDeleted->europe );
-    free( toBeDeleted );
+   removePlayer( toBeDeleted->Lord_Godalming );
+   removePlayer( toBeDeleted->Dr_Seward );
+   removePlayer( toBeDeleted->Van_Helsing );
+   removePlayer( toBeDeleted->Mina_Harker );
+   removePlayer( toBeDeleted->Dracula );
+   disposeMap( toBeDeleted->europe );
+   free( toBeDeleted );
 
 }
 
@@ -102,90 +99,90 @@ void disposeGameView(GameView toBeDeleted)
 // Get the current round
 Round getRound(GameView currentView)
 {
-    return currentView->RoundNum;
+   return currentView->RoundNum;
 }
 
 // Get the id of current player - ie whose turn is it?
 PlayerID getCurrentPlayer(GameView currentView)
 {
-    return currentView->CurrentPlayer;
+   return currentView->CurrentPlayer;
 }
 
 // Get the current score
 int getScore(GameView currentView)
 {
-    return currentView->GameScore;
+   return currentView->GameScore;
 }
 
 // Get the current health points for a given player
 int getHealth(GameView currentView, PlayerID player)
 {
-    switch(player) {
-	case PLAYER_LORD_GODALMING: return getPlayerHealth(currentView->Lord_Godalming);
-	case PLAYER_DR_SEWARD: 	    return getPlayerHealth(currentView->Dr_Seward);
-	case PLAYER_VAN_HELSING:    return getPlayerHealth(currentView->Van_Helsing);
-	case PLAYER_MINA_HARKER:    return getPlayerHealth(currentView->Mina_Harker);
-	case PLAYER_DRACULA: 	    return getPlayerHealth(currentView->Dracula);
-	default: 		    return 0;
-    }
+   switch(player) {
+      case PLAYER_LORD_GODALMING: return getPlayerHealth(currentView->Lord_Godalming);
+      case PLAYER_DR_SEWARD: 	    return getPlayerHealth(currentView->Dr_Seward);
+      case PLAYER_VAN_HELSING:    return getPlayerHealth(currentView->Van_Helsing);
+      case PLAYER_MINA_HARKER:    return getPlayerHealth(currentView->Mina_Harker);
+      case PLAYER_DRACULA: 	    return getPlayerHealth(currentView->Dracula);
+      default: 		    return 0;
+   }
 }
 
 // Get the current location id of a given player
 LocationID getLocation(GameView currentView, PlayerID player)
 {
 
-    switch(player) {
-	case 0: return getPlayerLocation(currentView->Lord_Godalming);
-	case 1: return getPlayerLocation(currentView->Dr_Seward);
-	case 2: return getPlayerLocation(currentView->Van_Helsing);
-	case 3: return getPlayerLocation(currentView->Mina_Harker);
-	case 4: return getPlayerLocation(currentView->Dracula);
-	default: return 0;
-    }
+   switch(player) {
+      case 0: return getPlayerLocation(currentView->Lord_Godalming);
+      case 1: return getPlayerLocation(currentView->Dr_Seward);
+      case 2: return getPlayerLocation(currentView->Van_Helsing);
+      case 3: return getPlayerLocation(currentView->Mina_Harker);
+      case 4: return getPlayerLocation(currentView->Dracula);
+      default: return 0;
+   }
 }
 
 //// Functions that return information about the history of the game
 
 // Fills the trail array with the location ids of the last 6 turns
 void getHistory(GameView currentView, PlayerID player,
-                            LocationID trail[TRAIL_SIZE])
-{
-    int i;
-    for (i=0; i<TRAIL_SIZE; i++){
-        if (player == 0) trail[i] = getPlayerHistory(currentView->Lord_Godalming, i);
-	else if (player == 1) trail[i] = getPlayerHistory(currentView->Dr_Seward, i);
-	else if (player == 2) trail[i] = getPlayerHistory(currentView->Van_Helsing, i);
-        else if (player == 3) trail[i] = getPlayerHistory(currentView->Mina_Harker, i);
-	else if (player == 4) trail[i] = getPlayerHistory(currentView->Dracula, i);
-    }
-    // may need to add some robustness here
+   LocationID trail[TRAIL_SIZE])
+   {
+      int i;
+      for (i=0; i<TRAIL_SIZE; i++){
+         if (player == 0) trail[i] = getPlayerHistory(currentView->Lord_Godalming, i);
+         else if (player == 1) trail[i] = getPlayerHistory(currentView->Dr_Seward, i);
+         else if (player == 2) trail[i] = getPlayerHistory(currentView->Van_Helsing, i);
+         else if (player == 3) trail[i] = getPlayerHistory(currentView->Mina_Harker, i);
+         else if (player == 4) trail[i] = getPlayerHistory(currentView->Dracula, i);
+      }
+      // may need to add some robustness here
 
-}
+   }
 
-//// Functions that query the map to find information about connectivity
+   //// Functions that query the map to find information about connectivity
 
-// Returns an array of LocationIDs for all directly connected locations
+   // Returns an array of LocationIDs for all directly connected locations
 
-LocationID *connectedLocations(GameView currentView, int *numLocations,
-                               LocationID from, PlayerID player, Round round,
-                               int road, int rail, int sea)
-{
-    int i,n=0;    
-    int *locations = malloc(71*sizeof(int));
-    int maxsteps = rail*((player+round) % 4);
-    if (!rail || maxsteps == 0) locations[n++] = from;
+   LocationID *connectedLocations(GameView currentView, int *numLocations,
+      LocationID from, PlayerID player, Round round,
+      int road, int rail, int sea)
+      {
+         int i,n=0;
+         int *locations = malloc(71*sizeof(int));
+         int maxsteps = rail*((player+round) % 4);
+         if (!rail || maxsteps == 0) locations[n++] = from;
 
-    for (i=0; i<=70; i++) {
-        if (road && connections(currentView->europe, from, i, ROAD)) {
-            locations[n++] = i;
-        } else if (sea && connections(currentView->europe, from, i, BOAT)) {
-            locations[n++] = i;
-        }
-    }  
-    if (rail && player!= 4) {
-            railConnections(currentView->europe, from, maxsteps, locations, &n); 
-    }
-    *numLocations = n;
-   
-    return locations;
-}
+         for (i=0; i<=70; i++) {
+            if (road && connections(currentView->europe, from, i, ROAD)) {
+               locations[n++] = i;
+            } else if (sea && connections(currentView->europe, from, i, BOAT)) {
+               locations[n++] = i;
+            }
+         }
+         if (rail && player!= 4) {
+            railConnections(currentView->europe, from, maxsteps, locations, &n);
+         }
+         *numLocations = n;
+
+         return locations;
+      }
--- HunterView.c
+++ HunterView.c
@@ -9,28 +9,26 @@
 //#include "Map.h"
 
 struct hunterView {
-    GameView gameView;
+   GameView gameView;
 };
-
 
 // Creates a new HunterView to summarise the current state of the game
 HunterView newHunterView(char *pastPlays, PlayerMessage messages[])
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    HunterView hunterView = malloc(sizeof(struct hunterView));
+   //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
+   HunterView hunterView = malloc(sizeof(struct hunterView));
 
-    hunterView->gameView = newGameView(pastPlays, messages);
+   hunterView->gameView = newGameView(pastPlays, messages);
 
-    return hunterView;
+   return hunterView;
 }
 
 
 // Frees all memory previously allocated for the HunterView toBeDeleted
 void disposeHunterView(HunterView toBeDeleted)
 {
-    //COMPLETE THIS IMPLEMENTATION
-    disposeGameView( toBeDeleted->gameView );
-    free( toBeDeleted );
+   disposeGameView( toBeDeleted->gameView );
+   free( toBeDeleted );
 }
 
 
@@ -39,51 +37,40 @@ void disposeHunterView(HunterView toBeDeleted)
 // Get the current round
 Round giveMeTheRound(HunterView currentView)
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    return getRound(currentView->gameView);
+   return getRound(currentView->gameView);
 }
 
 // Get the id of current player
 PlayerID whoAmI(HunterView currentView)
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    return getCurrentPlayer(currentView->gameView);
+   return getCurrentPlayer(currentView->gameView);
 }
 
 // Get the current score
 int giveMeTheScore(HunterView currentView)
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    return getScore(currentView->gameView);
+   return getScore(currentView->gameView);
 }
 
 // Get the current health points for a given player
 int howHealthyIs(HunterView currentView, PlayerID player)
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    return getHealth(currentView->gameView, player);
+   return getHealth(currentView->gameView, player);
 }
 
 // Get the current location id of a given player
 LocationID whereIs(HunterView currentView, PlayerID player)
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    //assuming turns counted from 1 not 0
-    /*if (player < turns) {
-       return UNKNOWN_LOCATION;
-    } else {
-    return currentView->ID[player];
-  }*/
-  return getLocation(currentView->gameView, player);
+   return getLocation(currentView->gameView, player);
 }
 
 //// Functions that return information about the history of the game
 
 // Fills the trail array with the location ids of the last 6 turns
 void giveMeTheTrail(HunterView currentView, PlayerID player,
-                            LocationID trail[TRAIL_SIZE])
+   LocationID trail[TRAIL_SIZE])
 {
-    getHistory(currentView->gameView, player, trail);
+   getHistory(currentView->gameView, player, trail);
 }
 
 //// Functions that query the map to find information about connectivity
@@ -91,18 +78,17 @@ void giveMeTheTrail(HunterView currentView, PlayerID player,
 // What are my possible next moves (locations)
 LocationID *whereCanIgo(HunterView currentView, int *numLocations, int road, int rail, int sea)
 {
-    //completed in dracview, will copy over when tested properly
-    return connectedLocations(currentView->gameView, numLocations,
-                              whereIs(currentView, whoAmI(currentView)), whoAmI(currentView),
-                              getRound(currentView->gameView), road, rail, sea);
+   return connectedLocations(currentView->gameView, numLocations,
+      whereIs(currentView, whoAmI(currentView)), whoAmI(currentView),
+      getRound(currentView->gameView), road, rail, sea);
 }
 
-// What are the specified player's next possible moves
+   // What are the specified player's next possible moves
 LocationID *whereCanTheyGo(HunterView currentView, int *numLocations,
-                           PlayerID player, int road, int rail, int sea)
+      PlayerID player, int road, int rail, int sea)
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    return connectedLocations(currentView->gameView, numLocations,
-                              whereIs(currentView, player), player,
-                              getRound(currentView->gameView), road, rail, sea);
+         //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
+   return connectedLocations(currentView->gameView, numLocations,
+         whereIs(currentView, player), player,getRound(currentView->gameView),
+         road, rail, sea);
 }
