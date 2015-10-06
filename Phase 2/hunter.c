644f69f35d433b96e1be7afe694e6458e2a5dc24
CeciliaWong <cecilia.w_5@hotmail.com>
2015-10-06 14:30:54 +1100

    Update hunter.c

--- Phase 2/hunter.c
+++ Phase 2/hunter.c
@@ -16,10 +16,21 @@ void decideHunterMove(HunterView gameState)
 - If found Dracula’s trail, how to decide which direction to take? Have all hunters move to possible locations of Dracula?
 - Have all hunters rest if haven’t found Dracula in (first six) rounds of game? Or if all hunters have HP less than ____?
 - Hunters should co-ordinate search through breadth-first search/ depth-first search/ something else?
-    Divided region into four parts, separation looks like wobbly 'H', so that if one player goes to hospital,
+    1.Divided region into four parts, separation looks like wobbly 'H', if one player goes to hospital,
     another player would take its place if allocated area is far away
+    2.Lord Godalming takes leftmost region, inclu. sea; Dr Seward takes middle upper-half; Van Helsing takes middle
+    lower-half; Mina Harker takes rightmost region  
+    3. Don't know how to hunt down Dracula
+    4. Have hunters rest at ports (if possible) if blood points < 6(Dracula MAY less likely travel by sea [lose blood points])
+    5. 
     */
     // TODO ...
     // Replace the line below by something better
+    char areaOne[]={"MA","GR","AL","BA","SR","SN","LS","CA","GW","DU","ED","MN","LV","SW","LO","PL"}; //then DU, GW
+    //if (giveMeTheRound(currentView) == 0) 
+    char areaTwo[]={"LE","NA","BR","PA","ST","NU","PR","BR","LI","FR","CO","HA","AM","BU"};
+    char areaThree[]={"FL","RO","BI","NP","GO","CG","MR","TO","BO","CF","GE","ZU","MI","VE"};
+    char areaFour[]={"AT","VA","SR","BE","SZ","ZA","VI","BD","KL","CD","GA","BC","CN","VR","SO","SA"};
+    
     registerBestPlay("GE","I'm on holiday in Geneva");
 }
