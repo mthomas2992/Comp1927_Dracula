cf437a7105ac85b86fc55a2ffc93a9cb227b6da0
CeciliaWong <cecilia.w_5@hotmail.com>
2015-10-24 12:09:03 +1100

    Deleted unecessary stuff

--- Phase 2/hunter.c
+++ Phase 2/hunter.c
@@ -16,11 +16,6 @@ void decideHunterMove(HunterView gameState)
     another player would take its place if allocated area is far away
     2.Lord Godalming takes leftmost region, inclu. sea; Dr Seward takes middle upper-half; Van Helsing takes middle
     lower-half; Mina Harker takes rightmost region
-    3. atm did breadth-first search
-    Need to do:
-    4. Don't know how to hunt down Dracula
-    5. Have hunters rest at ports (if possible) if blood points < 6(Dracula MAY less likely travel by sea [lose blood points])
-    6.  need to implement what happens if in hospital
     */
 
     //Dublin and Galway visited twice
@@ -75,53 +70,7 @@ void decideHunterMove(HunterView gameState)
 		registerBestPlay(place, "he'll never hear me comin'");
 	}
 }
-    //previously used mod round number to determine which city player should
-    //visit, but player needs to rest so use array, keep track of city
-    /*int locationNo[NUM_PLAYERS-2] = {0};
-    char place;
 
-    if (whoAmI(gameState) == PLAYER_LORD_GODALMING) {
-        if (locationNo[PLAYER_LORD_GODALMING] == 17) { //do you want me to get rid of magic no?
-            locationNo[PLAYER_LORD_GODALMING] = 0;
-        } else {
-            locationNo[PLAYER_LORD_GODALMING]++;
-        }
-        int i = locationNo[PLAYER_LORD_GODALMING];
-        place = *areaOne[i];
-
-    } else if (whoAmI(gameState) == PLAYER_DR_SEWARD) {
-
-        if (locationNo[PLAYER_DR_SEWARD] == 13) {
-            locationNo[PLAYER_DR_SEWARD] = 0;
-        } else {
-            locationNo[PLAYER_DR_SEWARD]++;
-        }
-        int i = locationNo[PLAYER_DR_SEWARD];
-        place = *areaTwo[i];
-
-    } else if (whoAmI(gameState) == PLAYER_VAN_HELSING) {
-
-        if (locationNo[PLAYER_VAN_HELSING] == 13) {
-            locationNo[PLAYER_VAN_HELSING] = 0;
-        } else {
-            locationNo[PLAYER_VAN_HELSING]++;
-        }
-        int i = locationNo[PLAYER_VAN_HELSING];
-        place = *areaThree[i];
-
-    } else {
-
-        if (locationNo[PLAYER_MINA_HARKER] == 15) {
-            locationNo[PLAYER_MINA_HARKER] = 0;
-        } else {
-            locationNo[PLAYER_MINA_HARKER]++;
-        }
-        int i = locationNo[PLAYER_MINA_HARKER];
-        place = *areaFour[i];
-    }
-
-    char* move = &place;
-*/
     //Temporary random location code
 /*    if (giveMeTheRound(gameState)==0){
 		registerBestPlay("BC","Matt is super cool");
